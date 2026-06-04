96719786ca31633320a8401fdb9db9fc92e8542f
ABHISHEK KUMAR <abhishekkumarpand@gmail.com>
2026-06-04 17:37:25 +0530

    Add files via upload

---
+++ assignment.java
@@ -0,0 +1,13 @@
+package optator;
+public class assignment {
+    public static void main(String args[]) {
+        int A = 10;
+        A +=10;
+     int B=10;
+        B -=10;
+        System.out.println(A);
+        System.out.println(B);
+        
+    }
+    
+}
