49f3953953676f432d5b384d280ae5668b254a29
ABHISHEK KUMAR <abhishekkumarpand@gmail.com>
2026-06-07 00:13:25 +0530

    Add files via upload

---
+++ unay.java
@@ -0,0 +1,13 @@
+package optator;
+
+
+public class unay {
+    public static void main(String arrgs[]){
+        int a  = 10;
+        int b  = a--;
+        System.out.println(a);
+        System.out.println(b);
+    }
+
+    
+}
