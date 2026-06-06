419c589b1441a094fab4dc6852f974f4a7029009
ABHISHEK KUMAR <abhishekkumarpand@gmail.com>
2026-06-07 00:13:01 +0530

    Add files via upload

---
+++ relationall.java
@@ -0,0 +1,15 @@
+package optator;
+// Source code is decompiled from a .class file using FernFlower decompiler.
+public class relationall {
+   public static void main(String args[]) {
+      int var1 = 10;
+      int var2 = 20;
+      System.out.println(var1 == var2);
+      System.out.println(var1 != var2);
+      System.out.println(var1 > var2);
+      System.out.println(var1 < var2);
+      System.out.println(var1 >= var2);
+      System.out.println(var1 <= var2);
+      System.out.println(var2);
+   }
+}
