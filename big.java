fe51e5e522998bb2b51b904e60492c5953340bd3
ABHISHEK KUMAR <abhishekkumarpand@gmail.com>
2026-06-08 21:17:30 +0530

    Add files via upload

---
+++ big.java
@@ -0,0 +1,14 @@
+public class big {
+    public static void main(String args[]){
+        int A = 1;
+        int B = 3;
+        if( A >= B){
+            System.out.println(true);
+
+        }
+        else{
+        System.out.println(false);
+        }
+    }
+    
+}
